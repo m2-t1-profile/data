483a0667fc85895b1689fc5404296e17508f867b
K-connor <k.connor614@gmail.com>
2019-11-05 18:10:06 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="flex content-between bg-gray-500 w-full h-screen">
+    <div class="relative container w-1/3 bg-gray-100">
+      <nuxt-link to="/profile/connor" class="rounded h-full text-grey-darkest no-underline shadow-md">
+        <img class="absolute object-cover w-full h-full" src="/Connor.jpg" title="To Profile">
+      </nuxt-link>
+      <div class="absolute z-10 text-gray-100 font-bold bottom-0">
+        <div class="text-3xl">
+          JING COUNTY
+        </div>
+        <div class="text-5xl">
+          MINTAE-KIM
+        </div>
+      </div>
+    </div>
+    <div class="relative w-1/3 bg-gray-400">
+      <!--      <div class="absolute z-10">HIIIIII</div>-->
+      <!--      <nuxt-link to="/profile/connor">-->
+      <!--        <img class="absolute object-cover w-full h-full cursor-pointer" src="/Connor.jpg">-->
+      <!--      </nuxt-link>-->
+    </div>
+    <div class="w-1/3 bg-gray-300">
+      <!--      <nuxt-link to="/user2">user2</nuxt-link>-->
+      <!--      <img class="object-cover w-full h-full cursor-pointer" src="/Connor.jpg">-->
+    </div>
+
+  </div>
+</template>
+
+<script>
+  // import TInput from 'vue-tailwind/src/elements/TInput.vue';
+
+  export default {
+    name: 'index',
+
+    components: {
+      // TInput,
+    },
+
+    data: () => ({
+      model: '',
+    }),
+  };
+</script>
+
+<style>
+
+
+</style>
---
+++ pages/profile/connor.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="w-full m-0 p-0 bg-cover bg-bottom">
+    <div class="container max-w-4xl mx-auto pt-16 md:pt-32 text-center break-normal">
+      <p class="text-black font-extrabold text-3xl md:text-5xl">{{ ment }}</p>
+      <p class="text-xl md:text-2xl text-gray-500">{{ subMent }}</p>
+
+      <div class="flex flex-wrap mt-6 pb-16 lg:pb-0 w-4/5 lg:w-full mx-auto items-center justify-between">
+        <a class="link" href="#" data-tippy-content="@connor">
+          <svg class="">
+            <title>github</title>
+            <path v-bind:d="simpleIcons.get('github').path"></path>
+          </svg>
+        </a>
+        <a class="link" href="#" data-tippy-content="@connor">
+          <svg class="">
+            <title>github</title>
+            <path v-bind:d="simpleIcons.get('github').path"></path>
+          </svg>
+        </a>
+        <a class="link" href="#" data-tippy-content="@connor">
+          <svg class="">
+            <title>github</title>
+            <path v-bind:d="simpleIcons.get('github').path"></path>
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  // import simpleIcons from 'simple-icons';
+
+  export default {
+    name: 'connor',
+    layout: 'baseLayout',
+
+    data: () => ({
+      simpleIcons: simpleIcons,
+      ment: '안녕하세요! 만나뵙게되어 영광입니다.',
+      subMent: '환영합니다!',
+    }),
+  };
+</script>
+
+<style scoped>
+
+</style>
